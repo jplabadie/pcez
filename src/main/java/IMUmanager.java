82aef619f4661993097cc3d6074292f15a2c8c97
jplabadie <jhl42@nau.edu>
2018-03-04 03:14:29 -0700

    Add casting to bytes, change package

--- src/main/java/IMUmanager.java
+++ src/main/java/IMUmanager.java
@@ -1,20 +1,24 @@
-package java;
+
+import com.pi4j.io.i2c.I2CBus;
+import com.pi4j.io.i2c.I2CDevice;
 import com.pi4j.io.i2c.I2CFactory;
-import com.pi4j.io.i2c.impl.I2CDeviceImpl;
 
 import java.io.IOException;
-import java.lang.System;
 
 /**
  * @author Jean-Paul Labadie
  */
 public class IMUmanager {
 
+    private static I2CBus bus ;
+    private static I2CDevice bgi_mag;
+    private static I2CDevice bgi_acc;
+
     public static void main(String[] args) throws InterruptedException, IOException, I2CFactory.UnsupportedBusNumberException {
 
-        private static I2CBus bus = I2CFactory.getInstance(I2CBus.Bus_1);
-        private static I2CDevice bgi_mag = bus.getDevice(0x1c);
-        private static I2CDevice bgi_acc = bus.getDevice(0x6a);
+        bus = I2CFactory.getInstance(I2CBus.BUS_1);
+        bgi_mag = bus.getDevice(0x1c);
+        bgi_acc = bus.getDevice(0x6a);
 
         enableMag();
         for(int i=0; i < 20; i++){
@@ -25,25 +29,37 @@ public class IMUmanager {
     }
 
     private static void enableMag(){
-        writeMagReg( 0x24, 0b11110000); // temp sensor enable, magnetometer hires 50Hz
-        writeMagReg( 0x25, 0b01100000); // full scale to +/- 12 Gauss
-        writeMagReg( 0x26, 0b00000000); // set magnetometer to continuous conversion mode
+        writeMagReg( (byte) 0x24, (byte)0b11110000); // temp sensor enable, magnetometer hires 50Hz
+        writeMagReg((byte) 0x25,(byte) 0b01100000); // full scale to +/- 12 Gauss
+        writeMagReg((byte) 0x26, (byte)0b00000000); // set magnetometer to continuous conversion mode
     }
 
     private static void enableAcc(){
-        writeAccReg( 0x24, 0b01100111);
-        writeAccReg( 0x25, 0b00100000);
+        writeAccReg((byte) 0x24,(byte) 0b01100111);
+        writeAccReg( (byte)0x25,(byte) 0b00100000);
     }
 
     private static void writeMagReg( byte register, byte value ){
-        int result = big_mag.write(register,value);
+        try {
+           bgi_mag.write(register,value);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void writeAccReg( byte register, byte value ){
-        int result = bgi_acc.write(register,value);
+        try {
+            bgi_acc.write(register,value);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static int readMagReg( int register ){
-        bgi_mag.read( register );
+        try {
+            return bgi_mag.read( register );
+        } catch (IOException e) {
+            return -1;
+        }
     }
 }
